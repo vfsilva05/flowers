8f3f12e301729f7cd42e1b001ecdac0f12b652c3
unknown <victor_ferreira.silva@hotmail.com>
2024-02-05 12:24:10 -0300

    up

---
+++ main.js
@@ -0,0 +1,7 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+document.addEventListener("DOMContentLoaded", function() {
+        const loveMessage = document.querySelector(".love-message");
+        loveMessage.classList.add("pulse");
+    });
